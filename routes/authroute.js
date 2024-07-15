9da432b0bce95a13ebd88b7404adba9f34827ac9
SatviK1608 <satvik1608@gmail.com>
2024-07-15 22:15:04 +0530

    update

--- routes/authroute.js
+++ routes/authroute.js
@@ -3,6 +3,9 @@ const router  = express.Router();
 const {login ,signup ,logout} = require("../Controller/authcontr")
 
 
+router.get('/',(req,res)=>{
+    res.json("Hello")
+})
 router.post('/login' , login)
 
 router.get('/logout' ,logout )
